1e85741911889b295989fe6915244b4b38c450c5
SahilTalwarTOR <sahiltalwar2004@gmail.com>
2025-01-01 19:48:47 -0500

    Added file checking and created apiobj, added input validation too. That'll be important.

---
+++ APIObj.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vs_iAPI
+{
+    class APIObj
+    {
+
+        internal string apikey = "";
+        internal string name = "";
+        internal string frequency;
+        internal Boolean running = false;
+
+
+        public APIObj(string apikey, string name, string frequency)
+        {
+            this.apikey = apikey;
+            this.name = name;
+            this.frequency = frequency;
+            this.running = false; // Always on object initialization it should not be running.
+        }
+
+        internal string Apikey { get { return apikey; } 
+            set { apikey = value; }
+        }
+        internal string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        internal string Frequency
+        {
+            get { return frequency; }
+            set { frequency = value; }
+        }
+
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,62 @@
 ﻿using System.Reflection.Metadata;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace vs_iAPI
 {
+
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        string frequency = "";
+        string api_name = "";
+        internal string api_key = ""; // Theres probably someone on GitHub who just searched "api_key" and thinks they'll find something here LOL
+        Boolean akcExists = false;
 
         public MainPage()
         {
             InitializeComponent();
+
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                string appDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                if (File.Exists(appDataDirectory + "/akc.dat"))
+                {
+                    akcExists = true;
+                }
+                else
+                {
+                    akcExists = false;
+                    File.Create(Path.Combine(appDataDirectory, "akc.dat"));
+                    // The file exists, so we'll probablyyy have to load it. We'll do that later on
+
+                }
+            }
         }
 
         private void nameCompleted(object sender, EventArgs e)
         {
             doTextHide(entryName, checkName);
+            this.api_name = entryName.Text;
         }
 
+        private void pickedFrequency(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                this.frequency = picker.Items[selectedIndex];
+            }
+
+
+        }
         private void keyCompleted(object sender, EventArgs e)
         {
             doTextHide(entryKey, checkKey);
+            this.api_key = entryKey.Text;
         }
 
         private void doTextHide(Entry textbox, CheckBox checkbox)
@@ -34,6 +72,34 @@ namespace vs_iAPI
                 textbox.IsPassword = true;
             }
         }
+
+	private void saveAPI(Object sender, EventArgs e) {
+		// We'll do some of the data-saving here, it's going to be quite a function..
+        // We have important variables to use!
+	}
+
+        private void saveCheck(Object sender, EventArgs e)
+        {
+            // Variables to check
+            string localApi = this.api_key.Trim();
+            string localName = this.api_name.Trim();
+            string localFreq = this.frequency.Trim();
+
+            if (localApi == "" || localName == "" || localFreq == "")
+            {
+                // Well, things aren't looking good, the user entered some empty parameters. Not okay! We have two options
+                // 1. We respectfully tell the user to re-enter details
+                // 2. We empty all fields, make the program crash, and make them re-launch everything, maybe we even lock them out for 2 hours, should I create a support hotline for them to call? I'll be the one picking up calls and I will make sure its as frustrating at possible
+                // The second one may be best, but for my sanity, we will choose the first one.
+
+                throw new ArgumentException("Missing inputs");
+            } else
+            {
+                // Okay, we good :D
+                APIObj enteredApi = new APIObj(this.api_key, this.api_name, this.frequency);
+            }
+        }
+	
     }
 
 }
